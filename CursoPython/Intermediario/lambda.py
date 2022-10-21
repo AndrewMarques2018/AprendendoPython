03a20edef594787e4146a836a2c1ce2fb13cbd14
Andrew Marques Silva <53877651+AndrewMarques2018@users.noreply.github.com>
2022-10-21 09:45:28 -0300

    tuplas e lambdas

---
+++ CursoPython/Intermediario/lambda.py
@@ -0,0 +1,26 @@
+# Expressões Lambda
+# lambda simples, multiplicacao
+a = lambda x, y: x*y
+print(a(2,4))
+print()
+
+
+# ordenar uma lista
+lista = [
+    ['p1', 13],
+    ['p2', 6],
+    ['p3', 7],
+    ['p4', 50],
+    ['p5', 8]
+]
+# metodo funcao
+"""""
+def func(item):
+    return item[1]
+
+lista.sort(key=func, reverse=True)
+"""
+
+# metodo lambda
+lista.sort(key=lambda item: item[1], reverse=True)
+print(lista)
---
+++ CursoPython/Intermediario/tuplas.py
@@ -0,0 +1,11 @@
+# Tuplas - lista em que os valores não são modificados
+t1 = (1,2,3,4,5)
+t2 = 1,2,3,4,5
+t3 = t1 + t2
+
+print(t1, type(t1))
+print(t2, type(t2))
+print(t3, type(t3))
+
+t4 = (1,2,3)*10
+print(t4)
