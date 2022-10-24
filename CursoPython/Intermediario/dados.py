27c02aa7bb2daaa3707b9596d37c5d7a1d7d457e
Andrew Marques Silva <53877651+AndrewMarques2018@users.noreply.github.com>
2022-10-24 10:04:55 -0300

    mapeamentos

---
+++ CursoPython/Intermediario/dados.py
@@ -0,0 +1,40 @@
+produtos = [
+    {"preco": 100},
+    {"preco": 55.5},
+    {"preco": 30.98},
+    {"preco": 80.0},
+    {"preco": 50},
+    {"preco": 40},
+    {"preco": 30.80},
+    {"preco": 50.50},
+    {"preco": 20.65},
+    {"preco": 10},
+    {"preco": 5},
+    {"preco": 15.40},
+    {"preco": 70.33},
+    {"preco": 87.13},
+    {"preco": 22.45}
+]
+
+pessoas = [
+    {'nome': 'Larissa', 'idade': 33},
+    {'nome': 'Lara', 'idade': 40},
+    {'nome': 'Luana', 'idade': 12},
+    {'nome': 'Luciana', 'idade': 5},
+    {'nome': 'Laura', 'idade': 20},
+    {'nome': 'Leticia', 'idade': 17},
+    {'nome': 'Livia', 'idade': 18},
+    {'nome': 'Ludimila', 'idade': 2},
+    {'nome': 'Lulu', 'idade': 55},
+    {'nome': 'Lauane', 'idade': 78},
+    {'nome': 'Lili', 'idade': 22},
+    {'nome': 'Liliane', 'idade': 21},
+    {'nome': 'Lauriete', 'idade': 18},
+    {'nome': 'Lidia', 'idade': 23},
+    {'nome': 'Lorena', 'idade': 51},
+    {'nome': 'Leila', 'idade': 27}
+]
+
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+
---
+++ CursoPython/Intermediario/mapeamento.py
@@ -0,0 +1,29 @@
+"""
+Importando listas, utilizando map
+"""
+
+from dados import produtos, pessoas, lista
+
+nova_lista = map(lambda x: x * 2, lista)
+# nova_lista = [x*2 for x in lista]
+
+print(lista)
+print(list(nova_lista))
+
+def aumentar_preco(p):
+    p['preco'] = round(p['preco']*1.10, 2)
+    return p
+
+produtos_novos = map(aumentar_preco, produtos)
+
+print(produtos)
+print(list(produtos_novos))
+
+def aumentar_idade(p):
+    p['idade'] = round(p['idade']*1.10)
+    return p
+
+pessoas_novas = map(aumentar_idade, pessoas)
+
+print(pessoas)
+print(list(pessoas_novas))
