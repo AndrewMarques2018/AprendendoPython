0f64f7f814433af3cd30ea21c884447e9cc723c3
Andrew Marques Silva <53877651+AndrewMarques2018@users.noreply.github.com>
2022-10-24 14:32:21 -0300

    modulos

---
+++ CursoPython/Intermediario/modulo.py
@@ -0,0 +1,15 @@
+def saudacao():
+    print("olá")
+
+
+def multiplica(x, y):
+    return x * y
+
+
+def soma(x, y):
+    return x + y
+
+
+# executado apenas se esse modulo for executado diretamente
+if __name__ == '__main__':
+    print("main")
---
+++ CursoPython/Intermediario/utilizando_modulos.py
@@ -0,0 +1,5 @@
+import modulo
+
+modulo.saudacao()
+print(modulo.soma(2, 5))
+print(modulo.multiplica(5, 3))
